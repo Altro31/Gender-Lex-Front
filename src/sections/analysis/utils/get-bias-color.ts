d8cb3b14263bedeeec7d8016edac4a742c16bda1
Altro31 <albe020531@outlook.com>
2024-11-12 14:45:22 -0500

    fix: astro server-islands bug

--- src/sections/analysis/utils/get-bias-color.ts
+++
@@ -1,8 +0,0 @@
-export function getBiasColor(
-    biasRate: number,
-    suffix: string = "stroke",
-): string {
-    if (biasRate < 0.3) return `${suffix}-chart-2`
-    if (biasRate < 0.7) return `${suffix}-chart-4`
-    return `${suffix}-chart-1`
-}
