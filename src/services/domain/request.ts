839614b36438798d958cddf309fdfd5f071d6810
Altro31 <albe020531@outlook.com>
2024-11-14 16:26:23 -0500

    feat: analysis historial

--- src/services/domain/request.ts
+++ src/services/domain/request.ts
@@ -5,11 +5,44 @@ import type { z } from "astro:content"
 
 type RequestCreateInput = z.infer<typeof schemas.RequestCreateSchema>
 
+interface FindAllArgs {
+    page?: number
+    limit?: number
+}
+
 export class RequestRepository {
     static async create(data: RequestCreateInput, request: Request) {
         const user = await getUser(request)
         if (!user) return
         const prisma = getPrisma(user)
         return prisma.request.create({ data })
+    }
+
+    static async findOne(id: string, request: Request) {
+        const user = await getUser(request)
+        if (!user) return
+        const prisma = getPrisma(user)
+
+        return prisma.request.findUnique({ where: { id } })
+    }
+
+    static async findAll(
+        { limit = 5, page = 0 }: FindAllArgs,
+        request: Request,
+    ) {
+        const user = await getUser(request)
+        if (!user) return
+        const prisma = getPrisma(user)
+
+        return prisma.request.findMany({
+            take: limit,
+            orderBy: { createdAt: "desc" },
+            select: {
+                id: true,
+                createdAt: true,
+                originalText: true,
+                Analysis: true,
+            },
+        })
     }
 }
