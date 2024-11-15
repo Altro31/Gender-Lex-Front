63d26943257b590c860d150615346a135513725a
Altro31 <albe020531@outlook.com>
2024-11-15 02:02:43 -0500

    refactor: node 20 support

--- src/services/pdf/extract-pdf-text.ts
+++ src/services/pdf/extract-pdf-text.ts
@@ -39,8 +39,6 @@ export async function extractPDFText(fileURL: string) {
 
     const streamAsset = await pdfServices.getContent({ asset: resultAsset })
 
-    const { promise, resolve } = Promise.withResolvers<string>()
-
     let buffer = Buffer.alloc(0) // Inicializar buffer vacío
     // Crear un WritableStream que escriba en el buffer
     const writableStream = new Writable({
@@ -49,19 +47,20 @@ export async function extractPDFText(fileURL: string) {
             callback()
         },
     }) // Leer el stream y escribirlo en el buffer
-    streamAsset.readStream.pipe(writableStream).on("finish", () => {
-        // Una vez que se complete la escritura en el buffer, crear el zip en memoria
-        const zip = new AdmZip(buffer) // Ahora puedes trabajar con el zip en memoria
-        console.log("Zip creado en memoria") // Por ejemplo, podrías extraer los archivos aquí
-        let jsondata = zip.readAsText("structuredData.json")
-        let data = JSON.parse(jsondata)
-        const text = data.elements.reduce(
-            (acc: string, curr: any) =>
-                curr.Text ? acc + "\n" + curr.Text : acc,
-            "",
-        )
-        resolve(text)
-    })
 
-    return promise
+    return new Promise<string>((resolve) => {
+        streamAsset.readStream.pipe(writableStream).on("finish", () => {
+            // Una vez que se complete la escritura en el buffer, crear el zip en memoria
+            const zip = new AdmZip(buffer) // Ahora puedes trabajar con el zip en memoria
+            console.log("Zip creado en memoria") // Por ejemplo, podrías extraer los archivos aquí
+            let jsondata = zip.readAsText("structuredData.json")
+            let data = JSON.parse(jsondata)
+            const text = data.elements.reduce(
+                (acc: string, curr: any) =>
+                    curr.Text ? acc + "\n" + curr.Text : acc,
+                "",
+            )
+            resolve(text)
+        })
+    })
 }
