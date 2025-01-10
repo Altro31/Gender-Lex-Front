bef84a013ab13d45ffd89596e00071be3476eea1
Altro <albe020531@outlook.com>
2025-01-09 20:51:09 -0500

    feat: update API to process multiple dataset entries and save results to a specific directory

--- src/pages/api/test/index.ts
+++ src/pages/api/test/index.ts
@@ -2,14 +2,23 @@ import type { GenderLexType } from "@/schemas/gender-lex"
 import { genderLex } from "@/services/domain/gender-lex"
 import type { APIRoute } from "astro"
 import fs from "fs"
+import dataset from "test/dataset.json"
 
 function saveFile(object: GenderLexType) {
     const jsonString = JSON.stringify(object, null, 2)
-    fs.writeFileSync(`${object.originalText.slice(0, 20)}.json`, jsonString)
+    fs.writeFileSync(
+        `test/results/${object.originalText.slice(0, 20)}.json`,
+        jsonString,
+    )
 }
 
 export const GET: APIRoute = async () => {
-    const result = await genderLex("Todas son iguales")
-    saveFile(result.object)
-    return new Response("Hola")
+    const res = []
+    for (const data of dataset) {
+        const { object } = await genderLex(data)
+        res.push(object)
+        console.log("Fulfilled")
+    }
+
+    return new Response(JSON.stringify(res))
 }
